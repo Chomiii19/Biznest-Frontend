c7a68806a9ce1e7f8ac2f91913de77571a2b4bda
Chomiii19 <jomariborines55@gmail.com>
2025-06-30 23:54:38 +0800

    Remove unused Manrope font families from Tailwind config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,13 +11,6 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [fontsLoaded, error] = useFonts({
-    "Manrope-Bold": require("../assets/fonts/Manrope-Bold.ttf"),
-    "Manrope-ExtraBold": require("../assets/fonts/Manrope-ExtraBold.ttf"),
-    "Manrope-ExtraLight": require("../assets/fonts/Manrope-ExtraLight.ttf"),
-    "Manrope-Light": require("../assets/fonts/Manrope-Light.ttf"),
-    "Manrope-Medium": require("../assets/fonts/Manrope-Medium.ttf"),
-    "Manrope-Regular": require("../assets/fonts/Manrope-Regular.ttf"),
-    "Manrope-Semibold": require("../assets/fonts/Manrope-SemiBold.ttf"),
     "Roboto-Bold": require("../assets/fonts/Roboto-Bold.ttf"),
     "Roboto-ExtraBold": require("../assets/fonts/Roboto-ExtraBold.ttf"),
     "Roboto-Medium": require("../assets/fonts/Roboto-Medium.ttf"),
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,12 +14,6 @@ module.exports = {
         "icon-stroke": "#848483",
       },
       fontFamily: {
-        mBold: ["Manrope-Bold", "sans-serif"],
-        mExtrabold: ["Manrope-ExtraBold", "sans-serif"],
-        mExtralight: ["Manrope-ExtraLight", "sans-serif"],
-        mLight: ["Manrope-Light", "sans-serif"],
-        mRegular: ["Manrope-Regular", "sans-serif"],
-        mSemibold: ["Manrope-SemiBold", "sans-serif"],
         rBold: ["Roboto-Bold", "sans-serif"],
         rExtrabold: ["Roboto-ExtraBold", "sans-serif"],
         rMedium: ["Roboto-Medium", "sans-serif"],
