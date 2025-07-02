fb8a3f9d1b810c44aaa7ebb505c2315ed18b47f9
Chomiii19 <jomariborines55@gmail.com>
2025-07-02 16:36:22 +0800

    Update screen transition animations

--- app/(screens)/_layout.tsx
+++ app/(screens)/_layout.tsx
@@ -2,8 +2,60 @@ import { Stack } from "expo-router";
 
 export default function ScreensLayout() {
   return (
-    <Stack
-      screenOptions={{ headerShown: false, animation: "slide_from_bottom" }}
-    />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen
+        name="changePassword"
+        options={{ animation: "slide_from_right" }}
+      />
+      <Stack.Screen
+        name="contactUs"
+        options={{ animation: "slide_from_right" }}
+      />
+      <Stack.Screen
+        name="createPost"
+        options={{ animation: "slide_from_bottom" }}
+      />
+      <Stack.Screen
+        name="editProfile"
+        options={{ animation: "slide_from_right" }}
+      />
+      <Stack.Screen name="FAQ" options={{ animation: "slide_from_right" }} />
+      <Stack.Screen
+        name="feedback"
+        options={{ animation: "slide_from_right" }}
+      />
+      <Stack.Screen
+        name="messages"
+        options={{ animation: "slide_from_bottom" }}
+      />
+      <Stack.Screen
+        name="notifications"
+        options={{ animation: "slide_from_bottom" }}
+      />
+      <Stack.Screen
+        name="ReportIssue"
+        options={{ animation: "slide_from_right" }}
+      />
+      <Stack.Screen
+        name="selectLocation"
+        options={{ animation: "slide_from_right" }}
+      />
+      <Stack.Screen
+        name="settings"
+        options={{ animation: "slide_from_bottom" }}
+      />
+      <Stack.Screen
+        name="viewConversation"
+        options={{ animation: "slide_from_right" }}
+      />
+      <Stack.Screen
+        name="viewPost"
+        options={{ animation: "slide_from_right" }}
+      />
+      <Stack.Screen
+        name="viewProfile"
+        options={{ animation: "slide_from_right" }}
+      />
+    </Stack>
   );
 }
